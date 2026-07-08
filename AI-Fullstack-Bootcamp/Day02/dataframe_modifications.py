3092e84d7d2109afc0f3cdd15cc42ef5d5afc083
rabbanishaik1421 <rabbanishaik1421@gmail.com>
2026-07-08 06:00:47 +0530

    day 2 was partially done

---
+++ AI-Fullstack-Bootcamp/Day02/dataframe_modifications.py
@@ -0,0 +1,80 @@
+#=============================================================#
+#Data Frame Modifications ====================================#
+#=============================================================#
+import pandas as pd
+
+employees = {
+    "EmpId":[101, 102, 103, 104, 105],
+    "Name":["Shaik", "Rabbani", "Harsha", "Priya", "Madhu"],
+    "Salary":[21000, 28000, 45000, 60000, 75000]
+}
+
+df = pd.DataFrame(employees)
+
+#Add Column with value assignment
+df["Country"] = "India"
+
+#Add column with calculation
+df["Bonus"] = df["Salary"] * 0.05
+
+#Add a column with a list
+df["Gender"] = ["Male", "Male", "Female", "Female", "Male"]
+
+#Add Multiple columns
+df["TaxAmount"] = df["Salary"] * 0.02
+df["NetSalary"] = df["Salary"] - df["TaxAmount"]
+
+df2 = df.assign(Bonus2=df["Salary"] * 0.05)
+# print(df2)
+
+#insert
+df.insert(3, "Department", ["IT", "IT", "HR", "HR", "Finance"])
+# print(df)
+
+
+def category(salary):
+    if salary > 25000:
+        return "Senior"
+    else:
+        return "Junior"
+
+#using apply    
+df["Category"] = df["Salary"].apply(category)
+
+df.loc[:, "Experience"] = [3, 5, 6, 7, 8]
+
+def change_country(experience):
+    """
+    Returns city based on experiece
+    """
+    if experience <= 3:
+        return "Hyderabad"
+    elif experience <= 5:
+        return "Delhi"
+    else:
+        return "Bangalore"    
+
+df["Country"] = df["Experience"].apply(change_country)
+
+def performance(salary):
+    """
+    Returns performance report based on salary
+    """
+    if salary < 25000:
+        return "Average"
+    elif salary <= 50000:
+        return "Good"
+    else:
+        return "Excellent"
+    
+df["Performance"] = df["Salary"].apply(performance)
+
+# df.groupby("Department")
+
+#Delete Column
+# df = df.drop("Performance", axis=1)
+# df.drop("Performance", axis=1, inplace=True)
+
+print("\nEmployee Data")
+print(df)
+# print(edata)
